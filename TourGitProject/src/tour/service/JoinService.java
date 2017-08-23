89c68db9928deaf6c314aa40d2e10adf6d8cf680
kosta <kosta@kosta-HP>
2017-08-23 18:39:28 +0900

    20170823 18:39 by DG(2)

--- TourGitProject/src/tour/service/JoinService.java
+++ TourGitProject/src/tour/service/JoinService.java
@@ -1,5 +1,6 @@
 package tour.service;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 import tour.dao.JoinDao;
@@ -9,56 +10,85 @@ import tour.exception.MyException;
 public class JoinService implements Service {
 
 	@Override
-	public void execute(Scanner scan) {
+	public void execute(Scanner sc) {
 		while (true) {
 			System.out.println("==========================================================");
 			System.out.println("|이용약관, 개인정보 수집 및 이용, 위치정보 이용약관(선택)|");
 			System.out.println("|, 프로모션 안내, 메일 수신(선택)에 모두 동의합니까?(Y/N)|");
 			System.out.println("==========================================================");
 			System.out.print("입력 : ");
-			char yn = scan.nextLine().charAt(0);
+			char yn = sc.nextLine().charAt(0);
 			switch (yn) {
 			case 'Y':
 			case 'y':
 				System.out.println("회원가입을 위한 필수입력 항목을 입력해 주십시오");
 				System.out.println("===============================================");
-				String email = "";
+				String email;
 				while (true) {
 					System.out.print("E-Mail 주소 입력 : ");
-					email = scan.nextLine();
-
-					 int at = 0;
-					 int dot = 0;
-					 for (int i = 0; i < email.length(); i++) {
-					 if (email.charAt(i) == '@') {
-					 at++;
-					 }
-					 if (email.charAt(i) == '.') {
-					 dot++;
-					 }
-					 }
-					 if (dot == 1 && at == 1) {
-					 break;
-					 } else {
-					 System.out.println("다시 입력하세요");
-					 }
+					email = sc.nextLine();
+					if (JoinDao.emailCheck(email)) {
+						break;
+					} else {
+						System.out.println("이메일 형식이 틀립니다. 다시 입력하세요");
+					}
 				}
-
 				System.out.println("===============================================");
-				System.out.print("패스워드 입력 : ");
-				String pwd = scan.nextLine();
+				String pwd;
+				while (true) {
+					System.out.print("패스워드 입력(영문+숫자, 6~10자리) : ");
+					pwd = sc.nextLine();
+					if (JoinDao.pwdCheck(pwd)) {
+						break;
+					} else {
+						System.out.println("영문 + 숫자 또는 6~10자리로 입력하세요");
+					}
+				}
 				System.out.println("===============================================");
-				System.out.print("이름 입력 : ");
-				String name = scan.nextLine();
+				String name;
+				while (true) {
+					System.out.print("이름 입력 : ");
+					name = sc.nextLine();
+					if (JoinDao.nameAndAddrCheck(name)) {
+						break;
+					} else {
+						System.out.println("한글만 입력하세요");
+					}
+				}
 				System.out.println("===============================================");
-				System.out.print("생년월일 입력(ex170822) : ");
-				String birth = scan.nextLine();
+				String birth;
+				while (true) {
+					System.out.print("생년월일 입력(ex20170822) : ");
+					birth = sc.nextLine();
+					if (JoinDao.birthCheck(birth)) {
+						break;
+					} else {
+						System.out.println("잘못입력하셨습니다. 다시입력하세요");
+					}
+				}
 				System.out.println("===============================================");
-				System.out.print("주소 입력(시까지 입력) : ");
-				String addr = scan.nextLine();
+				String addr;
+				while (true) {
+					System.out.print("주소 입력(시까지 입력) : ");
+					addr = sc.nextLine();
+					if (JoinDao.nameAndAddrCheck(addr)) {
+						break;
+					} else {
+						System.out.println("한글만 입력하세요");
+					}
+				}
 				System.out.println("===============================================");
-				System.out.print("핸드폰 번호 입력(-제외) : ");
-				String cellNum = scan.nextLine();
+				String cellNum;
+				while (true) {
+					System.out.print("핸드폰 번호 입력(-제외) : ");
+					cellNum = sc.nextLine();
+					if (JoinDao.cellNumCheck(cellNum)) {
+						break;
+					} else {
+						System.out.println("핸드폰번호 형식이 다릅니다. 다시입력하세요");
+					}
+				}
+				
 				try {
 					int result = JoinDao.insertMem(new MemDto(email, pwd, name, birth, addr, cellNum));
 					if (result != 0) {
@@ -67,6 +97,8 @@ public class JoinService implements Service {
 					}
 				} catch (MyException e) {
 					System.out.println(e.getMessage());
+				} catch (NoSuchAlgorithmException e) {
+					e.printStackTrace();
 				}
 				break;
 			case 'N':
@@ -77,5 +109,5 @@ public class JoinService implements Service {
 				System.out.println("잘못입력하셨습니다. Y 또는 N을 입력하세요");
 			}
 		}
-	}
-}
+	} // end of execute
+} // end of class
