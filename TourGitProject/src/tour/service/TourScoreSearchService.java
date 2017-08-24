1aaf3fa2f5b8dcbdad12713775eb31b283afa6c7
kosta <kosta@kosta-HP>
2017-08-24 12:33:57 +0900

    2017/08/24 12:33 yong

--- TourGitProject/src/tour/service/TourScoreSearchService.java
+++ TourGitProject/src/tour/service/TourScoreSearchService.java
@@ -32,10 +32,17 @@ public class TourScoreSearchService implements Service{
       if(size==0){
     	  System.out.println(" 회원 평가가 존재하지 않습니다. ");
       }else{
-    	  System.out.printf(
-    	            "-----------------------------------------------\n 교통 : %.1f\n 숙박 : %.1f\n 시설 : %.1f\n 음식 : %.1f\n 기타 : %.1f\n-----------------------------------------------\n",
-    	            traffic,stay,sisul,food,etc
-    	            );
+//    	  System.out.printf(
+//    	            "-----------------------------------------------\n 교통 : %.1f\n 숙박 : %.1f\n 시설 : %.1f\n 음식 : %.1f\n 기타 : %.1f\n-----------------------------------------------\n",
+//    	            traffic,stay,sisul,food,etc
+//    	            );
+    	  System.out.println("-----------------------------------------------");
+    	  System.out.println(" 교통 : "+getStar(traffic/size));
+    	  System.out.println(" 숙박 : "+getStar(stay/size));
+    	  System.out.println(" 시설 : "+getStar(sisul/size));
+    	  System.out.println(" 음식 : "+getStar(food/size));
+    	  System.out.println(" 기타 : "+getStar(etc/size));
+    	  System.out.println("-----------------------------------------------");
     	  for (int i = 0; i < list.size(); i++) {
               System.out.println(
                     list.get(i).getId().substring(0, 3)+"**** : "+list.get(i).getAssessment()
@@ -43,6 +50,22 @@ public class TourScoreSearchService implements Service{
               
            }
       }
+   }
+   
+   public String getStar(double d){
+	   StringBuilder sb = new StringBuilder();
+	   double temp = d;
+	   
+	   int frontDot = (int)temp;
+	   double backDot = d-frontDot;
+	   
+	   for (int i = 0; i < frontDot; i++) {
+		   sb.append("★");
+	   }
+	   if( backDot >= 0.5 ){
+		   sb.append("☆");
+	   }
+	   return new String(sb);
    }
 }
 
